74aa8c3d0bbc5236de64cfb9a398aa1060f6568c
David Quennoz <105565781+david-qz@users.noreply.github.com>
2022-06-05 11:09:46 -0700

    Keep defering initialization until the broswer has sized our img

--- app.js
+++ app.js
@@ -7,51 +7,60 @@ class BouncingLogo {
     static velFactor = 12;
 
     static get randomVelocity() {
-        const vel = BouncingLogo.velocities[ Math.floor(Math.random() * this.velocities.length) ];
+        const vel = Object.assign({}, BouncingLogo.velocities[Math.floor(Math.random() * this.velocities.length)]);
         vel.x *= BouncingLogo.velFactor;
         vel.y *= BouncingLogo.velFactor;
         return vel;
     }
 
     static get randomPosition() {
-        return { x: 40 + Math.random() * 15, y: 40 + Math.random() * 15 };
+        return { x: 30 + Math.random() * 30, y: 30 + Math.random() * 30 };
     }
 
     constructor(pos, vel, scale = 1) {
         this.img = document.createElement('img');
         this.img.src = BouncingLogo.filename;
         this.img.classList.add(BouncingLogo.elementClass);
-        this.widthPercent = BouncingLogo.defaultWidth * scale;
-        this.img.style.width = this.widthPercent + '%';
+        this.width = BouncingLogo.defaultWidth * scale;
+        this.img.style.width = this.width + '%';
 
         this.pos = pos || BouncingLogo.randomPosition;
         this.vel = vel || BouncingLogo.randomVelocity;
-        this.hueRotation = 0;
+        this.hueRotation = Math.floor(Math.random() * 6) * 60;
 
-        // Defer initialization of these values until the element in the in the DOM and
+        // Defer initialization of these values until the element is the in the DOM and
         // has gone through layout.
         this.initialized = false;
         this.bounds = null;
         this.screenAspectRatio = null;
         this.aspectRatio = null;
+        this.height = null;
 
         document.body.appendChild(this.img);
+        this.parent = this.img.parentElement;
     }
 
     #initialize() {
-        this.screenAspectRatio = this.img.parentElement.clientWidth / this.img.parentElement.clientHeight;
+        this.screenAspectRatio = this.parent.clientWidth / this.parent.clientHeight;
         this.aspectRatio = this.img.clientWidth / this.img.clientHeight;
+        this.height = this.width / this.aspectRatio * this.screenAspectRatio;
+
+        // Sometimes we're too early and the browser hasn't ran lay out on the img
+        // element yet.
+        if (this.height <= 0) return;
+
         this.bounds = {
             xMin: 0 + BouncingLogo.screenPorch,
             yMin: 0 + BouncingLogo.screenPorch,
-            xMax: 100 - this.widthPercent - BouncingLogo.screenPorch,
-            yMax: 100 - (this.widthPercent / this.aspectRatio * this.screenAspectRatio) - BouncingLogo.screenPorch,
+            xMax: 100 - this.width - BouncingLogo.screenPorch,
+            yMax: 100 - this.height - BouncingLogo.screenPorch,
         };
         this.initialized = true;
     }
 
     tick(deltaTime) {
         if (!this.initialized) this.#initialize();
+        if (!this.initialized) return;
 
         this.#handleBounds();
 
